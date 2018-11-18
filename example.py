970c0dc01017599b90ae4291631ae784efab7ff0
Leo Cai <zhbn.cai@gmail.com>
2018-11-18 15:14:33 +0800

    Show image and text

---
+++ example.py
@@ -0,0 +1,16 @@
+import sys
+
+import cv2
+from PySide import QtGui
+
+from imageviewer import ImageViewer
+
+
+app = QtGui.QApplication(sys.argv)
+v = ImageViewer()
+v.setWindowTitle("Image Viewer Example")
+v.show()
+img1 = cv2.imread("pictures/1.jpg")
+v.set_image(img1)
+v.add_text("txt1", "Text Example", color=(255, 255, 255), position=(0, 0))
+sys.exit(app.exec_())
---
+++ imageviewer.py
@@ -0,0 +1,120 @@
+# coding:utf-8
+"""
+A image viewer based on PySide shows images from openCV.
+
+Python: ver 3.6.6. or above
+Required Modules: PySide, numpy, openCV(3.2)
+Author: Leo Cai
+"""
+
+import sys
+import time
+from typing import List, Dict, Tuple
+
+import cv2
+import numpy as np
+from PySide.QtGui import QApplication, QWidget, QGraphicsScene, QGraphicsView, QVBoxLayout, QHBoxLayout, QToolBar
+from PySide.QtGui import QImage, QPixmap, QGraphicsTextItem, QGraphicsPixmapItem, QColor
+from PySide.QtCore import Qt
+
+
+__version__ = "0.1"
+
+
+class ImageViewer(QWidget):
+    def __init__(self):
+        super(ImageViewer, self).__init__()
+        self._image = np.array([])
+        self._rois: List[Roi] = []
+        self._texts: Dict[str, QGraphicsTextItem] = {}
+        self._show_rois = False
+        self._show_texts = False
+
+        # layout
+        self.v_box = QVBoxLayout()
+        self.setLayout(self.v_box)
+
+        # image view
+        self.pix_map_item = QGraphicsPixmapItem()
+        self.scene = QGraphicsScene()
+        self.scene.setSceneRect(0, 0, 0, 0)
+        self.scene.addItem(self.pix_map_item)
+        self.view = QGraphicsView(self.scene)
+        self.v_box.addWidget(self.view)
+        self.view.setMinimumSize(400, 300)
+        self.view.setAlignment(Qt.AlignCenter)
+        self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        # tool bar
+        self.toolbar = QToolBar()
+        self.v_box.addWidget(self.toolbar)
+
+    def paintEvent(self, *args, **kwargs):
+        self.refresh()
+
+    def refresh(self):
+        if self._image.any():
+            self.set_image(self._image)
+
+    def get_image(self):
+        return self._image
+
+    def set_image(self, img: np.ndarray):
+        if isinstance(img, np.ndarray):
+            shape = img.shape
+            if len(shape) == 2:
+                # gray scale image
+                self._image = img
+                im = cv2.cvtColor(self._image, code=cv2.COLOR_GRAY2RGB)
+
+            elif len(shape) == 3 and (shape[2] == 3):
+                # color image
+                self._image = img
+                im = img
+
+            else:
+                raise TypeError("Image must be an openCV image(gray scale or BGR)!")
+
+        else:
+            raise TypeError("Image must be an openCV image(numpy ndarray)!")
+
+        # update image in the scene
+        h, w, _ = im.shape
+        bytes_per_line = w*3
+        im = QImage(im.data, w, h, bytes_per_line, QImage.Format_RGB888).rgbSwapped()   # bgr to rgb
+        w, h = self.view.width(), self.view.height()
+        pix_map = QPixmap.fromImage(im).scaled(w, h, Qt.KeepAspectRatio)
+        self.pix_map_item.setPixmap(pix_map)
+
+        # keep the scene in the center of the view
+        bounds = self.scene.itemsBoundingRect()
+        self.view.setSceneRect(bounds)
+
+    def add_text(self, name: str,
+                 txt: str, *,
+                 color: Tuple[int, int, int]=(0, 0, 0),
+                 position: Tuple[int, int] = (0, 0)
+                 ):
+        item: QGraphicsTextItem = self.scene.addText(txt)
+        item.setPos(position[0], position[1])
+        item.setDefaultTextColor(QColor(color[0], color[1], color[2]))
+        self._texts[name] = item
+
+    def update_text(self, name: str, txt: str):
+        self._texts[name].setPlainText(txt)
+
+    def remove_text(self, name):
+        self.scene.removeItem(self._texts[name])
+        self._texts.pop(name)
+
+
+class Roi(object):
+    def __init__(self, x, y, width, height, shape='oval', name=''):
+        super(Roi, self).__init__()
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.shape = shape
+        self.name = name
